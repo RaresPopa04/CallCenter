2f8e2d3f9a7a558cd15321cc27e63b2ba876073a
Popa Rares Andrei <R.A.Popa@student.tudelft.nl>
2024-06-15 00:45:35 +0200

    I takes input from the user

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const wss = new WebSocket.Server({ server });
-
+const axios = require("axios");
 const path = require("path");
-
 require("dotenv").config();
 
 const speech = require("@google-cloud/speech");
@@ -13,7 +12,7 @@ const client = new speech.SpeechClient({
   keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS
 });
 
-//Configure Transcription Request
+// Configure Transcription Request
 const request = {
   config: {
     encoding: "MULAW",
@@ -23,7 +22,14 @@ const request = {
   interimResults: true,
 };
 
+let headers = "";
+const callerTexts = {};
+let streamSids = [];
+let connectionCount = 0;
+
 wss.on("connection", function connection(ws) {
+  let callerText = "";
+  let streamSid = "";
   console.log("New Connection Initiated");
 
   let recognizeStream = null;
@@ -36,11 +42,15 @@ wss.on("connection", function connection(ws) {
         break;
       case "start":
         console.log(`Starting Media Stream ${msg.streamSid}`);
+        streamSid = msg.streamSid;
         recognizeStream = client
           .streamingRecognize(request)
           .on("error", console.error)
           .on("data", (data) => {
-            console.log(data.results[0].alternatives[0].transcript);
+            if (data.results[0] && data.results[0].isFinal) {
+              console.log(data.results[0].alternatives[0].transcript);
+              callerText += data.results[0].alternatives[0].transcript;
+            }
             wss.clients.forEach((client) => {
               if (client.readyState === WebSocket.OPEN) {
                 client.send(
@@ -54,30 +64,60 @@ wss.on("connection", function connection(ws) {
           });
         break;
       case "media":
-        recognizeStream.write(msg.media.payload);
+        if (recognizeStream) {
+          recognizeStream.write(Buffer.from(msg.media.payload, "base64"));
+        }
         break;
       case "stop":
-        console.log(`Call Has Ended`);
-        recognizeStream.destroy();
+        if (recognizeStream) {
+          recognizeStream.end();
+          callerTexts[streamSid] = callerText;
+        }
         break;
+    }
+  });
+
+  ws.on("close", () => {
+    if (recognizeStream) {
+      recognizeStream.end();
     }
   });
 });
 
+app.use(express.json());
+
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 app.post("/", (req, res) => {
+  connectionCount++;
+  headers = req.headers.host;
+  console.log(req.body);
   res.set("Content-Type", "text/xml");
-
   res.send(`
     <Response>
       <Start>
         <Stream url="wss://${req.headers.host}/"/>
       </Start>
-      <Say>I will stream the next 60 seconds of audio through your websocket</Say>
-      <Pause length="60" />
+      <Say>Hi</Say>
+      <Pause length="4" />
+      <Redirect method="POST">/next-response?streamSid=${req.body.StreamSid}</Redirect>
+    </Response>
+  `);
+});
+
+app.post("/next-response", (req, res) => {
+  console.log(req.query);
+  const streamSid = req.query.streamSid;
+  console.log(streamSid);
+  const callerText = callerTexts[streamSid];
+  console.log(callerText);
+  res.set("Content-Type", "text/xml");
+  res.send(`
+    <Response>
+      <Say>Hi, I'm the next response. You said: ${callerText}</Say>
     </Response>
   `);
 });
