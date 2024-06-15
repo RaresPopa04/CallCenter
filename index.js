6f4bbd794cf817604934f34fc200c5c28a98a6f2
Popa Rares Andrei <R.A.Popa@student.tudelft.nl>
2024-06-16 00:46:53 +0200

    version of a repeating bot

--- index.js
+++ index.js
@@ -1,126 +1,107 @@
-const WebSocket = require("ws");
-const express = require("express");
-const app = express();
-const server = require("http").createServer(app);
-const wss = new WebSocket.Server({ server });
-const axios = require("axios");
-const path = require("path");
 require("dotenv").config();
+const express = require("express");
+const hbs = require("express-handlebars");
+const expressWebSocket = require("express-ws");
+const Transform = require("stream").Transform;
+const websocketStream = require("websocket-stream/stream");
+const WaveFile = require("wavefile").WaveFile;
+const AmazonTranscribeService = require("./transcribe-service");
+const TwilioClient = require("twilio");
 
-const speech = require("@google-cloud/speech");
-const client = new speech.SpeechClient({
-  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS
+const app = express();
+// extend express app with app.ws()
+expressWebSocket(app, null, {
+  perMessageDeflate: false,
 });
+app.engine("hbs", hbs());
+app.set("view engine", "hbs");
 
-// Configure Transcription Request
-const request = {
-  config: {
-    encoding: "MULAW",
-    sampleRateHertz: 8000,
-    languageCode: "en-GB",
-  },
-  interimResults: true,
-};
-
-let headers = "";
-const callerTexts = {};
-let streamSids = [];
-let connectionCount = 0;
-
-wss.on("connection", function connection(ws) {
-  let callerText = "";
-  let streamSid = "";
-  console.log("New Connection Initiated");
-
-  let recognizeStream = null;
-
-  ws.on("message", function incoming(message) {
-    const msg = JSON.parse(message);
-    switch (msg.event) {
-      case "connected":
-        console.log(`A new call has connected.`);
-        break;
-      case "start":
-        console.log(`Starting Media Stream ${msg.streamSid}`);
-        streamSid = msg.streamSid;
-        recognizeStream = client
-          .streamingRecognize(request)
-          .on("error", console.error)
-          .on("data", (data) => {
-            if (data.results[0] && data.results[0].isFinal) {
-              console.log(data.results[0].alternatives[0].transcript);
-              callerText += data.results[0].alternatives[0].transcript;
-            }
-            wss.clients.forEach((client) => {
-              if (client.readyState === WebSocket.OPEN) {
-                client.send(
-                  JSON.stringify({
-                    event: "interim-transcription",
-                    text: data.results[0].alternatives[0].transcript,
-                  })
-                );
-              }
-            });
-          });
-        break;
-      case "media":
-        if (recognizeStream) {
-          recognizeStream.write(Buffer.from(msg.media.payload, "base64"));
-        }
-        break;
-      case "stop":
-        if (recognizeStream) {
-          recognizeStream.end();
-          callerTexts[streamSid] = callerText;
-        }
-        break;
-    }
-  });
-
-  ws.on("close", () => {
-    if (recognizeStream) {
-      recognizeStream.end();
-    }
-  });
-});
-
-app.use(express.json());
-
+// <ake all the files in 'public' available
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
-
-app.post("/", (req, res) => {
-  connectionCount++;
-  headers = req.headers.host;
-  console.log(req.body);
-  res.set("Content-Type", "text/xml");
-  res.send(`
-    <Response>
-      <Start>
-        <Stream url="wss://${req.headers.host}/"/>
-      </Start>
-      <Say>Hi</Say>
-      <Pause length="4" />
-      <Redirect method="POST">/next-response?streamSid=${req.body.StreamSid}</Redirect>
-    </Response>
-  `);
+app.get("/", (request, response) => {
+  response.render("home", { number: process.env.TWILIO_NUMBER, layout: false });
 });
 
-app.post("/next-response", (req, res) => {
-  console.log(req.query);
-  const streamSid = req.query.streamSid;
-  console.log(streamSid);
-  const callerText = callerTexts[streamSid];
-  console.log(callerText);
-  res.set("Content-Type", "text/xml");
-  res.send(`
-    <Response>
-      <Say>Hi, I'm the next response. You said: ${callerText}</Say>
-    </Response>
-  `);
+// Responds with Twilio instructions to begin the stream
+app.post("/", (request, response) => {
+  response.setHeader("Content-Type", "application/xml");
+  response.render("twiml", { host: request.hostname, layout: false });
 });
 
-console.log("Listening on Port 8080");
-server.listen(8080);
+app.ws("/media", (ws, req) => {
+  // Audio Stream coming from Twilio
+  const mediaStream = websocketStream(ws);
+  let callSid;
+  const client = new TwilioClient();
+  const audioStream = new Transform({
+    transform: (chunk, encoding, callback) => {
+      const msg = JSON.parse(chunk.toString("utf8"));
+      if (msg.event === "start") {
+        callSid = msg.start.callSid;
+        console.log(`Captured call ${callSid}`);
+      }
+      // Only process media messages
+      if (msg.event !== "media") return callback();
+      // This is mulaw
+      return callback(null, Buffer.from(msg.media.payload, "base64"));
+    },
+  });
+  const pcmStream = new Transform({
+    transform: (chunk, encoding, callback) => {
+      const wav = new WaveFile();
+      wav.fromScratch(1, 8000, "8m", chunk);
+      wav.fromMuLaw();
+      return callback(null, Buffer.from(wav.data.samples));
+    },
+  });
+
+  const transcribeService = new AmazonTranscribeService(pcmStream);
+
+  // Pipe our streams together
+  mediaStream.pipe(audioStream).pipe(pcmStream);
+
+  transcribeService.on("transcription", (transcription) => {
+    console.log(`Processing ${transcription}`);
+    const twiml = new TwilioClient.twiml.VoiceResponse();
+    twiml.say(
+      {
+        voice: "Polly.Brian",
+        language: "en.GB",
+      },
+      transcription
+    );
+
+
+    // if(transcription.includes("menu")){
+    //   twiml.say({
+    //     voice: "Polly.Brian",
+    //     language: "en.GB",
+    //   },
+    //   "Our menu includes: pizza, pasta, and salad. What would you like to order?"
+    // )
+    // }
+    twiml.pause({ length: 120 });
+    client.calls(callSid).update({
+    twiml: twiml.toString(),
+    });
+  });
+
+  mediaStream.on("close", () => {
+    transcribeService.stop();
+  });
+});
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  console.trace(err);
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: {},
+  });
+});
+
+const listener = app.listen(8080, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
